a5e58745946b7825a53a2cab032995b3d67d9cef
andybond13 <andybond13@yahoo.com>
2017-11-04 21:30:06 -0700

    Move todo list to separate file.

--- DEM/run.sh
+++ DEM/run.sh
@@ -14,30 +14,3 @@ mpirun -np $np lmp_openmpi -e both -v r 0.003 -v insertSteps 200000 -in in.gener
 cd post
 lpp dump*generation
 cd ..
-
-
-#ffmpeg -r 1 -i data/input-%4d.png -pix_fmt yuv420p -r 30 data/output.mp4
-
-#contact status (generation*.dump): id p1x p1y p1z p2x p2y p2z p1id p2id periodic fx fy fz contactarea delta
-
-#######
-#TO DO:
-#create imagery for srdjan
-#(PY) contact boundary Z override 
-#(PY) voronoi boundary Z override 
-#(PY) connection to fabric tensor calculator
-#(PY) truss network to <voltage,thermal,mechanical> moose simulation 
-#(PY) voronoi network to <voltage,thermal,mechanical> moose simulation 
-#python:
-#  remove particles above certain height
-#  run until kinetic under limit
-#  compression simulations
-#ryan's insitu placement
-#######
-#DONE:
-#(PY) contact boundary conditions (physical groups)
-#(PY) voronoi boundary conditions (physical groups)
-#(PY) voronoi connection to liggghts data (voronoi.py)
-#(PY) voronoi connection to ethz data (voronoi.py)
-#(PY) contact dump to truss network (findContact.py)
-#(PY) element dataa (delta, contact area, contact force)
